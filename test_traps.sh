2b801d0595ebe5656f89f63370cbdbcba9d96e1c
David Banks <dave@hoglet.com>
2018-10-14 11:18:37 +0100

    Z80: Added a script to test for traps

---
+++ test_traps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# max number of half-cycles to simulate for
+n=3698250
+
+# Input file
+infile=z80basic_test.txt
+
+# Output file base
+outfile=z80basic_test
+
+echo "Z80 Trap Test"
+echo
+echo "Removal of a trap should not cause any failures"
+echo
+echo "Note: each test case takes ~10 minutes"
+echo
+echo "Generating reference bin/log files:"
+./z80basic -t ${outfile}.bin -n ${n} < ${infile} > ${outfile}.log
+md5sum ${outfile}.bin ${outfile}.log
+
+for i in 2866 3275 3343 3457 3470 3633 3659 3813 3823 4814 4908 5097 5137 5588 5750 5858 5901 6179 6359 6428
+do
+    echo "Testing for trap at t$i"
+    ./z80basic -x ${i} -t ${outfile}_${i}.bin -n ${n} < ${infile} > ${outfile}_${i}.log
+    md5sum ${outfile}_${i}.bin ${outfile}_${i}.log
+done
