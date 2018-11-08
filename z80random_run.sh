29f6a50218ec7633c47afb6c5fdec996acf37167
David Banks <dave@hoglet.com>
2018-11-08 17:57:28 +0000

    Z80: updated z80random_run.sh to use 4 cores

--- z80random_run.sh
+++ z80random_run.sh
@@ -1,14 +1,29 @@
 #!/bin/bash
 
-for i in $(seq 100 1000)
-do
-./z80random -u $i -t r.bin > r.out
-../Z80Decoder/decodez80 -d 0 -a -h -i -s2 -y --cpu nmos_zilog --phi= r.bin > r.log
-fail=`grep fail r.log | wc -l`
-warn=`grep -i warn r.log | wc -l`
-echo $i $fail $warn
-if [ "$fail" != "0" ] || [ "$warn" != "2" ]
-then
+# If called with no argumnets, spawn 4 children
+if [ $# -eq 0 ]; then
+    nohup ./z80random_run.sh 0 > z80random_run.log0 &
+    nohup ./z80random_run.sh 1 > z80random_run.log1 &
+    nohup ./z80random_run.sh 2 > z80random_run.log2 &
+    nohup ./z80random_run.sh 3 > z80random_run.log3 &
     exit
 fi
+
+for i in $(seq 1 1000)
+do
+
+    n=$(($i%4))
+
+    if [ "$n" == "$1" ]; then
+
+        ./z80random -u $i -t r$n.bin > r$n.out
+        ../Z80Decoder/decodez80 -d 0 -a -h -i -s2 -y --cpu nmos_zilog --phi= r$n.bin > r$n.log
+        fail=`grep fail r$n.log | wc -l`
+        warn=`grep -i warn r$n.log | wc -l`
+        echo $i $fail $warn
+        if [ "$fail" != "0" ] || [ "$warn" != "2" ]
+        then
+            exit
+        fi
+    fi
 done
